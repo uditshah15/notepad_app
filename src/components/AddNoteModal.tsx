17cfe76974265fcec76de7353f4371913f55d3e8
Udit Shah <uditshah15@gmail.com>
2025-07-09 08:09:33 +1200

    Use types instead of interface

--- src/components/AddNoteModal.tsx
+++ src/components/AddNoteModal.tsx
@@ -12,7 +12,7 @@ import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import { Textarea } from "@/components/ui/textarea";
 
-interface AddNoteModalProps {
+type AddNoteModalProps=  {
   onSave: (name: string, description: string) => void;
 }
 
